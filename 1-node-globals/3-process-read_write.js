759c3a2d106d96f2af3b5110e1c01c725e1eef66
aveepsit <aveepsitchowdhury@gmail.com>
2020-12-22 00:58:40 +0530

    updated process line clear and cursor move

--- 1-node-globals/3-process-read_write.js
+++ 1-node-globals/3-process-read_write.js
@@ -2,6 +2,12 @@
 process.stdout.write("Hello ");
 process.stdout.write("World!!!\n\n\n");
 
+// Advanced printing with stdout
+process.stdout.write("This line will be deleted!");
+process.stdout.clearLine(); // Cleans the current line in the stdout.
+process.stdout.cursorTo(0); // Takes the cursor to a specific char position.
+process.stdout.write("The line above is missing!");
+
 const questions = [
   "What is your Name?",
   "What would you rather be doing?",
@@ -14,6 +20,7 @@ const ask = (i = 0) => {
 };
 
 ask();
+
 // Stdin is a stream that takes input and created events on it.
 // data event is called whenever there is a data input event from the console
 // the on method is used to create handlers and without the process.exit it keeps on listening for input events.
